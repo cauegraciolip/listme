4159d0a78d5491555e04d59b556358414259605b
Caue Gracioli Palacio <caue.palacio@grupofleury.com.br>
2022-09-06 20:22:32 -0300

    feat: config pattern next components files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,29 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+
+import { globalStyle } from "../styles/global/documentStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  globalStyle();
+  return (
+    <>
+      <Head>
+        <title>ListMe!</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ fontFamily: "Open Sans" }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
 
 export default MyApp;
