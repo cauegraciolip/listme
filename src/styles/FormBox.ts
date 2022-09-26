c370517db12a9e71489443309c88e8724e44ddb0
Caue Gracioli Palacio <caue.palacio@grupofleury.com.br>
2022-09-26 20:26:39 -0300

    style: new global style

---
+++ src/styles/FormBox.ts
@@ -0,0 +1,12 @@
+import { styled } from "@stitches/react";
+
+export const FormBox = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  widht: "100%",
+  maxWidth: "500px",
+  minWidth: "300px",
+  backgroundColor: "#22436A",
+  borderRadius: 8,
+  padding: 20,
+});
