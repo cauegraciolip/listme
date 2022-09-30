0285f2122c8f59b2f45c33d64dd3ab0756b858ae
Caue Gracioli Palacio <caue.palacio@grupofleury.com.br>
2022-09-29 21:04:55 -0300

    feat: set useEffect to check user session

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,23 @@ import Header from "../components/Header";
 //STYLES
 import { Container } from "../styles/global/container";
 import { styled } from "@stitches/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const S_Div = styled("div", {
   background: "#516B8A",
 });
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
+  const routes = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      routes.push("/login");
+    }
+  }, [routes, session]);
+
   return (
     <S_Div>
       <Header />
