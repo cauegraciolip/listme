cf07d818956a5085cf7eaed5ba3241563a98adbe
Caue Gracioli Palacio <caue.palacio@grupofleury.com.br>
2022-10-04 21:29:01 -0300

    feat: new components styles

--- src/styles/global/textInputStyle.ts
+++ src/styles/global/textInputStyle.ts
@@ -1,5 +1,11 @@
 import { styled } from "@stitches/react";
-import { TextInput, NativeSelect, PasswordInput } from "@mantine/core";
+import {
+  TextInput,
+  NativeSelect,
+  PasswordInput,
+  Autocomplete,
+  Select,
+} from "@mantine/core";
 
 const StyledInput = styled(TextInput, {
   width: "100%",
@@ -18,6 +24,14 @@ const SelectStyled = styled(NativeSelect, {
   },
 });
 
+const SelectProper = styled(Select, {
+  marginBottom: 5,
+  "& > label": {
+    paddingLeft: 5,
+    color: "#F4F4F4",
+  },
+});
+
 const StyledPassword = styled(PasswordInput, {
   marginBottom: 5,
   "& > label": {
@@ -26,4 +40,18 @@ const StyledPassword = styled(PasswordInput, {
   },
 });
 
-export { StyledInput, SelectStyled, StyledPassword };
+const StyledAutoComplete = styled(Autocomplete, {
+  marginBottom: 5,
+  "& > label": {
+    paddingLeft: 5,
+    color: "#F4F4F4",
+  },
+});
+
+export {
+  StyledInput,
+  SelectStyled,
+  StyledPassword,
+  StyledAutoComplete,
+  SelectProper,
+};
