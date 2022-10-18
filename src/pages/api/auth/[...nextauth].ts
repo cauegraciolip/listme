0011228e87b2b9edd615b49d8a0429e38bffede0
Caue Gracioli Palacio <caue.palacio@grupofleury.com.br>
2022-10-18 20:25:14 -0300

    fix: database connection

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,14 +20,11 @@ export const authOptions = {
       name: "credentials",
       credentials: {},
       async authorize(credentials) {
-        const res = await fetch(
-          "https://listme.vercel.app/api/user/authenticate",
-          {
-            method: "POST",
-            body: JSON.stringify(credentials),
-            headers: { "Content-Type": "application/json" },
-          }
-        );
+        const res = await fetch("http://localhost:3000/api/user/authenticate", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
         const data = await res.json();
 
         if (!data.success) {
